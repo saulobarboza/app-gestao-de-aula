72840f5343e89be165a57fa4eb7ad2220539cfee
saulobarboza <saulo_barboza@yahoo.com.br>
2024-11-19 20:48:30 -0300

    Criando arquivos de estruturação e estilização da tela de atualização do tópico da aula

---
+++ app-gestao-de-aulas/src/pages/TopicUpdate/styles.js
@@ -0,0 +1,72 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+  arrowLeftBackButton: {
+    paddingHorizontal: 70,
+    paddingTop: Constants.statusBarHeight + 70,
+    backgroundColor: '#333333',
+  },
+
+  container: {
+    flex: 1,
+    paddingHorizontal: 70,
+    backgroundColor: '#333333',
+    justifyContent: 'center'
+  },
+
+  title: {
+    marginBottom: 70,
+    fontSize: 90,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+
+  formLabel: {
+    marginBottom: 30,
+    fontSize: 40,
+    lineHeight: 60,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    textAlign: 'left'
+  },
+
+  inputField: {
+    marginBottom: 70,
+    padding: 30,
+    width: 940,
+    height: 150,
+    borderRadius: 20,
+    borderStyle: 'solid',
+    fontSize: 40,
+    fontWeight: '400',
+    backgroundColor: '#FFFFFF'
+  },
+
+  inputFieldMessageError: {
+    marginBottom: 70,
+    fontSize: 32,
+    lineHeight: 60,
+    color: '#E02041',
+    fontWeight: 'bold',
+    textAlign: 'left'
+  },
+
+  submitButton: {
+    marginTop: 70,
+    height: 150,
+    borderRadius: 20,
+    borderWidth: 0,
+    backgroundColor: '#28E020',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  submitButtonText: {
+    fontSize: 40,
+    fontWeight: '600',
+    textTransform: 'uppercase',
+    color: '#FFFFFF',
+  }
+});
